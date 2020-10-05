208db45fc9adb722d448930cff462041a8fae9d7
João Pedro Henrique <joao.pedro.hsd@gmail.com>
2020-10-05 01:22:54 -0300

    Limitar tamanho das string para evitar erros nas migrations

--- src/IvaoBrasilDatabaseServiceProvider.php
+++ src/IvaoBrasilDatabaseServiceProvider.php
@@ -3,11 +3,13 @@
 namespace IvaoBrasil;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class IvaoBrasilDatabaseServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        Schema::defaultStringLength(191);
         $this->loadMigrationsFrom(__DIR__ . '/../migrations');
     }
 }
