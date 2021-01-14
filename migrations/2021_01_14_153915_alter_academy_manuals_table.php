fc25184bf6774338e8aecce1bf3aa72c6f3a73b4
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2021-01-14 12:43:44 -0300

    Corrigir erro no nome da classe da migration

--- migrations/2021_01_14_153915_alter_academy_manuals_table.php
+++ migrations/2021_01_14_153915_alter_academy_manuals_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAcademyCategoriesTagsTable extends Migration
+class AlterAcademyManualsTable extends Migration
 {
     /**
      * Run the migrations.
