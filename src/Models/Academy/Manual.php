00d53c277850e105b09aa1f6c365f34cbdef7f70
Joao Pedro Henrique <joao.pedro.hsd@gmail.com>
2021-01-14 10:16:21 -0300

    Add Manual model and migration

---
+++ src/Models/Academy/Manual.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace IvaoBrasil\Models\Academy;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class Manual extends Model
+{
+    protected $table = "academy_manuals";
+
+    protected $fillable = [
+        "title", "description", "author_vid", "language", "file_path", "is_visible", "created_at", "updated_at"
+    ];
+
+    protected $visible = [
+        "id", "title", "description", "author_vid", "language", "file_path", "is_visible", "created_at", "updated_at"
+    ];
+}
