f6efb074723ee9b60ac220186e41b00c9a064c93
Luciano Santos <luciano.ssants@gmail.com>
2021-01-22 21:03:53 -0300

    Add data to ais airports

---
+++ src/Models/Ais/AisAirport.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use IvaoBrasil\Models\Core\User;
+
+class AisAirport extends Model
+{
+    use HasFactory;
+
+    protected $visible = [
+        "id", "icao", "rwy_configuration", "rmk", "updated_by", "active"
+    ];
+
+    protected $fillable = [
+        "icao", "rwy_configuration", "rmk", "updated_by", "active"
+    ];
+
+    protected $table = "ais_airports";
+
+    
+    public function updatedBy() {
+        return $this->belongsTo(User::class);
+    }
+}
