7fb26439a7e961b580918ce5ef49514924e30c5a
Luciano <luciano.ssants@gmail.com>
2021-08-08 21:26:08 -0300

    change staff appointment

--- migrations/HQ/2021_08_09_002250_staff_appointment.php
+++ migrations/HQ/2021_08_09_002250_staff_appointment.php
@@ -4,23 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStaffAppointment extends Migration
+class StaffAppointment extends Migration
 {
-    /*
-     * Run the migrations
+    /**
+     * Run the migrations.
      *
      * @return void
-     * */
-
+     */
     public function up()
     {
         Schema::create('staff_appointment', function (Blueprint $table) {
-            $table->id();
-            $table->string('vid', 6);
-            $table->string('position', 16);
-            $table->dateTime('appointed_at')->useCurrent();
-            $table->enum('status', ['onTrial', 'effectived']);
-            $table->timestamps();
+           $table->id();
+           $table->string('vid', 6);
+           $table->string('position', 16);
+           $table->dateTime('appointed_at')->useCurrent();
+           $table->enum('status', ['onTrial', 'effectived']);
+           $table->timestamps();
         });
     }
 
