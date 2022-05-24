1afcf1dc0e2fb725e119ba5a26ebd0cac319d34a
Luciano Santos <luciano.ssants@gmail.com>
2022-05-24 18:41:44 -0300

    cast end_date

--- src/Models/Trainings/Trainings.php
+++ src/Models/Trainings/Trainings.php
@@ -7,6 +7,10 @@ use IvaoBrasil\Models\Core\User;
 
 class Trainings extends Model
 {
+    protected $casts = [
+        'end_date' => 'datetime:Y-m-d H:00',
+    ];
+    
     public function trainer() {
         return $this->belongsTo(User::class);
     }
