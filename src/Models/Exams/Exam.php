fc23b0848cec11f45de2710ae561b07362942c37
Joelson Carvalho <25236114+joelson-c@users.noreply.github.com>
2022-09-11 16:58:43 -0300

    feat(exams): add exam archiving

--- src/Models/Exams/Exam.php
+++ src/Models/Exams/Exam.php
@@ -10,12 +10,25 @@ class Exam extends Model
     protected $casts = [
         'end_date' => 'datetime:Y-m-d H:00',
     ];
-    
+
     public function examiner() {
         return $this->belongsTo(User::class);
     }
 
     public function member() {
         return $this->belongsTo(User::class);
+    }
+
+    public function getCanArchiveAttribute() {
+        $isClosed = $this->status === 'finished' || $this->status === 'cancelled';
+        return $isClosed && !$this->archived;
+    }
+
+    public function setArchivedAttribute($value) {
+        if (!$this->can_archive && $value) {
+            throw new \InvalidArgumentException('The exam could not be archived');
+        }
+
+        $this->attributes['archived'] = $value;
     }
 }
